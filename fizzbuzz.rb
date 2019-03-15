fa71e9d134476c3a207270cb945a6a9b06833f1c
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-03-15 19:45:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,10 +6,10 @@ def fizzbuzz(num)
     return "Fizz"
   elsif num % 5 == 0
     return "Buzz"
-  elsif num % 3== 0 && num % 5 == 0
+  elsif num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
   else 
-    return NIL
+    return nil
   end
 end
   
